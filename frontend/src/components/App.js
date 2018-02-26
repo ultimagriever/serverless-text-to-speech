c636e7cd0200a8fa44a19e80c5bc175f43700dc1
Pamela Argentino <pamela.oliveira@cognizant.com>
2018-02-26 00:03:58 -0300

    Add front-end code to the TTS app

---
+++ frontend/src/components/App.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { Container, Table, Row, Col } from 'reactstrap';
+import axios from 'axios';
+import PostForm from './PostForm';
+
+export default class App extends Component {
+  state = {
+    posts: []
+  };
+
+  async componentWillMount() {
+    const { data: posts } = await axios.get('https://jdf85x3umd.execute-api.us-east-1.amazonaws.com/prod/getPosts?postId=*');
+
+    this.setState({ posts });
+  }
+
+  createNewPost = async values => {
+    await axios.post('https://jdf85x3umd.execute-api.us-east-1.amazonaws.com/prod/newPost', values);
+
+    window.location.reload();
+  }
+
+  render() {
+    return (
+      <Container>
+        <Row className="mb-2">
+          <Col md="12">
+            <h1>Serverless Text-to-Speech App</h1>
+          </Col>
+          <Col md="12">
+            <p>
+              This proof-of-concept is a simple app designed to take a block of text and transform it into lifelike speech.
+              Currently, the languages supported are Danish, Dutch, English (Australian, British, Indian, American, Welsh),
+              French, Canadian French, German, Icelandic, Italian, Japanese, Korean, Norwegian, Portuguese, Brazilian Portuguese,
+              Romanian, Russian, Spanish (Castilian, American), Swedish, Turkish and Welsh.
+            </p>
+            <p>
+              It was entirely built with <a href="https://aws.amazon.com">Amazon Web Services</a> such as API Gateway,
+              DynamoDB, Lambda, SNS, Polly and S3.
+            </p>
+          </Col>
+        </Row>
+        <PostForm onSubmit={this.createNewPost} />
+        <Table>
+          <thead>
+            <tr>
+              <th>Status</th>
+              <th>Voice</th>
+              <th>Sound</th>
+            </tr>
+          </thead>
+          <tbody>
+          {
+            this.state.posts.map(post => (
+              <tr key={post.id}>
+                <td>{post.status}</td>
+                <td>{post.voice}</td>
+                <td>
+                  <audio controls>
+                    <source src={post.url} type="audio/mpeg" />
+                  </audio>
+                </td>
+              </tr>
+            ))
+          }
+          </tbody>
+        </Table>
+      </Container>
+    );
+  }
+}
---
+++ frontend/src/components/PostForm.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { Row, Col, Form, FormGroup, Label, Input, Button } from 'reactstrap';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
+import voices from './voices.json';
+
+export default class PostForm extends Component {
+  state = {
+    voice: null,
+    text: ''
+  };
+
+  handleChange = (name, value) => {
+    this.setState({
+      [name]: value
+    });
+  };
+
+  render() {
+    return (
+      <Form>
+        <Row className="mb-3">
+          <Col md="12">
+            <FormGroup>
+              <Label for="voice">
+                Voice
+              </Label>
+              <Select
+                options={voices}
+                searchable
+                name="voice"
+                onChange={(selected) => this.handleChange('voice', selected && selected.value)}
+                value={this.state.voice}
+              />
+            </FormGroup>
+          </Col>
+          <Col md="12">
+            <FormGroup>
+              <Label for="text">
+                Text
+              </Label>
+              <Input
+                type="textarea"
+                name="text"
+                value={this.state.text}
+                onChange={event => this.handleChange('text', event.target.value)}
+              />
+            </FormGroup>
+          </Col>
+          <Col md={{ size: '3', offset: '9' }} sm={{ size: '6', offset: '6' }}>
+            <Button type="button" onClick={() => this.props.onSubmit(this.state)} color="primary">
+              Add new post
+            </Button>
+          </Col>
+        </Row>
+      </Form>
+    )
+  }
+}
